dc8f17a188a4dbd4cebdbda3e29a4823728b474d
Sedat Atakan <satakan35@gmail.com>
2020-05-21 18:28:10 -0400

    commit

--- Burger/public/assets/js/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    
-  $(".devour-form").on("submit", function(event) {
-    //hint:setting up ajax put
-  });
-});
---
+++ public/assets/js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    
+  $(".devour-form").on("submit", function(event) {
+
+    var id = $(this).data("id");
+    var newSleep = $(this).data("newsleep");
+
+    var newSleepState = {
+      sleepy: newSleep
+    };
+
+
+
+
+    //hint:setting up ajax put
+
+
+
+
+
+
+  });
+});
