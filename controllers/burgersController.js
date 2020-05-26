ea42ca8c175d696bb4200a8e04ad4efeafeb880b
Sedat Atakan <satakan35@gmail.com>
2020-05-26 18:35:22 -0400

    last update

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -4,48 +4,40 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 // get route -> index
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
   res.redirect("/burgers");
 });
 
-router.get("/burgers", function(req, res) {
+router.get("/burgers", function (req, res) {
   //hint: burger.all
-  burger.all(function(data) 
-  {var hbsObject = {
-      burgers: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
+  burger.all(function (data) {
+   
+    res.render("index", {burger_data: data});
   });
 });
 
 // post route -> back to index
-  //hint: burger.create
-  router.post("/", function(req, res) {
-    res.redirect("/burgers"); 
-    });
-  
-  router.post("/burgers", function(req, res) {
-    burger.create(function(data) 
-    {var hbsObject = { 
-      burgers: data
-    };
-    res.render("index", hbsObject);
+//hint: burger.create
+// router.post("/", function(req, res) {
+//   res.redirect("/burgers");
+//   });
+
+router.post("/burgers/create", function (req, res) {
+  burger.create(req.body.burger_name, function (data) {
+   res.redirect("/")
   });
-  });
+});
 
 // put route -> back to index
-  //hint: burger.update()
-  // router.put("/", function(req, res) {
-  //   res.redirect("/burgers");
+//hint: burger.update()
+// router.put("/", function(req, res) {
+//   res.redirect("/burgers");
 
-  // router.put("/burgers", function(req, res) {
-  //   burger.update(function(data) 
-  //   {var hbsObject = {
-  //     burgers: data
-  //   };
-  //   res.render("index", hbrObject);
-  // });
-  // });
-  
+router.put("/burgers/:id", function (req, res) {
+  burger.update(req.params.id, function (data) {
+    
+    res.sendStatus(200) ;
+  });
+});
+
 module.exports = router;
