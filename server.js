76af26eb54465faa85975d86c918757abfdb7c8d
Sedat Atakan <satakan35@gmail.com>
2020-05-28 16:20:53 -0400

    heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 
 var PORT = process.env.PORT || 8000;
 var app = express();
-
+var port = process.env.PORT || 3000;
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
